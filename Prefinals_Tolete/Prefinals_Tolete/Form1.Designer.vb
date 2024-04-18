5f464318f408932c3ddf32e189e722b45430c9b5
RushiTolete <158274797+RushiTolete@users.noreply.github.com>
2024-04-19 04:49:32 +0800

    dbbooks
    Pre-Finals exam

---
+++ Prefinals_Tolete/Prefinals_Tolete/Form1.Designer.vb
@@ -0,0 +1,64 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        DataGridView1 = New DataGridView()
+        btn_seedata = New Button()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(36, 95)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowHeadersWidth = 51
+        DataGridView1.Size = New Size(630, 316)
+        DataGridView1.TabIndex = 0
+        ' 
+        ' btn_seedata
+        ' 
+        btn_seedata.Location = New Point(36, 45)
+        btn_seedata.Name = "btn_seedata"
+        btn_seedata.Size = New Size(94, 29)
+        btn_seedata.TabIndex = 2
+        btn_seedata.Text = "Show Data"
+        btn_seedata.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(709, 488)
+        Controls.Add(btn_seedata)
+        Controls.Add(DataGridView1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents btn_seedata As Button
+
+End Class
---
+++ Prefinals_Tolete/Prefinals_Tolete/Form1.vb
@@ -0,0 +1,52 @@
+﻿
+Imports MySql.Data.MySqlClient
+Public Class Form1
+
+
+    Public DBServer As String
+    Public DBPort As String
+    Public DBName As String
+    Public DBUser As String
+    Public DBPass As String
+    Public MysqlConn As New MySqlConnection
+    Public sqL As String
+    Public ds As New DataSet
+    Public cmd As MySqlCommand
+    Public dr As MySqlDataReader
+    Public da As MySqlDataAdapter
+    Public Pos As Integer
+
+    Private Sub btn_seedata_Click(sender As Object, e As EventArgs) Handles btn_seedata.Click
+        Dim MysqlConnect = New MySqlConnection
+        Dim MySqlCommand = New MySqlCommand
+        Dim MysqlAdpater = New MySqlDataAdapter
+        Dim MysqlTable = New DataTable
+
+        Dim DBSTRING As String = "server=127.0.0.1;" & "user id=root;" & "password=;" & "database=dbbooks;"
+        MysqlConnect.ConnectionString = DBSTRING
+        Dim SlectStrng As String = "SELECT id as 'Book ID', title as 'Title', genre as 'Genre', qty as 'Qty', price as 'Price' FROM book_tbl"
+
+
+        Try
+            MysqlConnect.Open()
+            MySqlCommand.Connection = MysqlConnect
+            MySqlCommand.CommandText = SlectStrng
+            MysqlAdpater.SelectCommand = MySqlCommand
+            MysqlAdpater.Fill(MysqlTable)
+            DataGridView1.DataSource = MysqlTable
+
+        Catch ex As Exception
+            MessageBox.Show("Cannot retrieve the data from Database. Error:" & ex.Message)
+            MysqlConnect.Close()
+        Finally
+            MysqlConnect.Close()
+
+        End Try
+    End Sub
+
+
+
+
+
+
+End Class
